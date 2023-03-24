2760fb52ea1fb639d0cc0550e14250f7b04637c7
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-03-24 12:49:02 +0100

    added imgs

--- src/App.jsx
+++ src/App.jsx
@@ -8,63 +8,73 @@ function App() {
   let availavblePokemonsList = [
     {
       id: 1,
-      name: 'Bulbasar',
+      name: 'Bulbasaur',
       cp: 439,
-      color: 'green'
+      color: 'green',
+      img: "src/assets/bulbasaur.webp"
     },
     {
       id: 2,
       name: 'Vulpix',
       cp: 194,
-      color: 'brown'
+      color: 'brown',
+      img: "src/assets/vulpix.webp"
     },
     {
       id: 3,
       name: 'Pikachu',
       cp: 455,
-      color: 'yellow'
+      color: 'yellow',
+      img: "src/assets/pikachu.webp"
     },
     {
       id: 4,
       name: 'Gyarados',
       cp: 2243,
-      color: 'pink'
+      color: 'pink',
+      img: "src/assets/gyarados.webp"
     },
     {
       id: 5,
       name: 'Pidgey',
       cp: 248,
-      color: 'blue'
+      color: 'blue',
+      img: "src/assets/pidgey.webp"
     },
     {
       id: 6,
       name: 'Wigglytuff',
       cp: 1362,
-      color: 'red'
+      color: 'red',
+      img: "src/assets/wigglytuff.webp"
     },
     {
       id: 7,
       name: 'Oddish',
       cp: 610,
-      color: 'black'
+      color: 'black',
+      img: "src/assets/oddish.webp"
     },
     {
       id: 8,
       name: 'Haunter',
       cp: 735,
-      color: 'white'
+      color: 'white',
+      img: "src/assets/haunter.webp"
     },
     {
       id: 9,
       name: 'Onix',
       cp: 1002,
-      color: 'purple'
+      color: 'purple',
+      img: "src/assets/onix.webp"
     },
     {
       id: 10,
       name: 'Exeggutor',
       cp: 2916,
-      color: 'beige'
+      color: 'beige',
+      img: "src/assets/exeggutor.webp"
     }
   ];
   // let chosenPokemonsList = []
--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,7 +1,7 @@
 function PokemonCard({pokemon, handleClick}) {
   return ( 
     <article className="pokemon-card" onClick={() => handleClick(pokemon)}>
-      <div style={{backgroundColor:pokemon.color}}></div>
+      <div style={{backgroundColor:pokemon.color}}><img src={pokemon.img} /> </div>
       <section>
       <h3>{pokemon.name}</h3>
       <p>{pokemon.cp} CP</p>
