89e8bde89f32a049f2eb92ae1225323e1564a277
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-03-24 12:54:45 +0100

    removed images

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,7 +1,9 @@
 function PokemonCard({pokemon, handleClick}) {
   return ( 
     <article className="pokemon-card" onClick={() => handleClick(pokemon)}>
-      <div style={{backgroundColor:pokemon.color}}><img src={pokemon.img} /> </div>
+      <div style={{backgroundColor:pokemon.color}}>
+        {/* <img src={pokemon.img} />  */}
+        </div>
       <section>
       <h3>{pokemon.name}</h3>
       <p>{pokemon.cp} CP</p>
